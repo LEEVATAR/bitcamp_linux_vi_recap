63fb796684ccee09b7631d83e33859d810d37a48
LEEVATAR <112069305+LEEVATAR@users.noreply.github.com>
2022-12-26 19:39:11 +0900

    day01/221221/javabasic

---
+++ Ex05Constant.java
@@ -0,0 +1,15 @@
+public class Ex05Constant {
+
+	public static void main(String[] args) {
+		// int 데이터타입 상수 NUMBER를 선언해보자
+		final int NUMBER;
+		// NUMBER에 5를 저장해보자
+		NUMBER = 5;
+		// 출력해보자
+		System.out.println(NUMBER);
+		// 변환시도해보자
+//		NUMBER = 123;
+//		System.out.println(NUMBER); //안된다.
+//		NUMBER = 5; 같은값 재지정도 안된다
+//		컴퓨터는 값을 저장할 때 기존 값과 같다고 해서 
+//		저장을 하지 않는다 라는것을 하지않는다.
