4a87139f97914cb2894ddca40c2d64fd3eb10198
LEEVATAR <112069305+LEEVATAR@users.noreply.github.com>
2022-12-26 19:38:14 +0900

    day01/221221/javabasic

---
+++ Ex04Variable.java
@@ -0,0 +1,16 @@
+public class Ex04Variable {
+
+	public static void main(String[] args) {
+		// int 데이터타입의 변수 number를 선언해보자
+		int number; //노란줄은 경고(옐로카드)같은것으로 실행은 가능함 빨간줄은 컴파일 자체가 안됨
+		//변수 number에 10을 저장해보자
+		number = 10;
+		// number 에 현재 저장된 값을 화면에 출력해보자
+		System.out.println(number);
+		//변수 number에 25를 저장해보자
+		number = 25;
+		// number에 현재 저장된 값을 화면에 출력해보자
+		System.out.println(number);
+	}
+
+}
